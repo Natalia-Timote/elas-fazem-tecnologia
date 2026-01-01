114016449aaa66b876003b762d79ac5f4f82b476
Natalia-Timote <nataliamirian.nt@gmail.com>
2026-01-01 01:40:58 -0300

    Iterando por uma lista

--- src/App.tsx
+++ src/App.tsx
@@ -38,29 +38,15 @@ function App() {
         <Logo image="./logo.png" title="Logo - Elas fazem tecnologia" />
       </header>
       <main>
-        <Banner image="./banner-home.jpg" title="Banner com ilustração de mulheres diversas utilizando tecnologia."></Banner>
-        <img src="./banner-home.jpg" alt="Banner com ilustração de mulheres diversas utilizando tecnologia." />
-        <EventForm>Preencha para criar um evento:</EventForm>
-        <div>
-          <section>
-            <Theme theme={themes[0]} />
-          </section>
-          <section>
-            <Theme theme={themes[1]} />
-          </section>
-          <section>
-            <Theme theme={themes[2]} />
-          </section>
-          <section>
-            <Theme theme={themes[3]} />
-          </section>
-          <section>
-            <Theme theme={themes[4]} />
-          </section>
-          <section>
-            <Theme theme={themes[5]} />
-          </section>
+        <div className='main-first'>
+          <Banner image="./banner-home.jpg" title="Banner com ilustração de mulheres diversas utilizando tecnologia."></Banner>
+          <EventForm>Preencha para criar um evento:</EventForm> 
         </div>
+        <section className='themes'>
+          {themes.map((theme) => {
+            return <Theme key={theme.id} theme={theme} />
+          })}
+        </section>
       </main>
     </>
   )
